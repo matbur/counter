a5f2f1d7c5cefe8a8c817a8b8f3d86c2dcd66da1
matbur95 <mateusz@burniak.pl>
2016-11-13 01:06:58 +0100

    Add image on the site instead of pdf file

--- web/server.py
+++ web/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+import subprocess
+
+from flask import Flask, flash, render_template, request
 from wtforms import Form, StringField
 
 from latex import create_pdf_file, create_tex_file
@@ -31,21 +33,26 @@ def parse_key(key):
 
 
 def parse_form(data):
-    print(sorted(data))
-    l = []
-    for k, v in data.items():
-        if v:
-            kk = parse_key(k)
-            kk.append(v)
-            l.append(tuple(int(i) for i in kk))
-    return l
+    parsed = []
+    for key, value in data.items():
+        if not value:
+            continue
+        parsed_key = parse_key(key) + [value]
+        parsed.append(tuple(int(i) for i in parsed_key))
+    return parsed
 
 
 def get_pdf(data, file):
     file += '.tex'
     moves = parse_form(data)
+    print(sorted(moves))
     create_tex_file(moves, file)
     create_pdf_file(file)
+
+
+def convert_pdf(file):
+    command = 'convert -density 300 {0}.pdf {0}.jpg'.format(file)
+    subprocess.call(command, shell=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -55,12 +62,30 @@ def index():
     if request.method == 'POST' and form.validate():
         data = form.data
         if any(data.values()):
-            get_pdf(data, ip)
             flash(parse_form(form.data))
+            get_pdf(data, ip)
+            convert_pdf(ip)
         else:
             flash('Serio?')
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, file=ip + '.pdf')
+    return render_template('index.html', form=form)
+
+
+@app.route('/file.tex')
+def get_tex_file():
+    file = request.remote_addr + '.tex'
+    return app.send_static_file(file)
+
+
+@app.route('/file.pdf')
+def get_pdf_file():
+    file = request.remote_addr + '.pdf'
+    return app.send_static_file(file)
+
+
+@app.route('/file.jpg')
+def get_jpg_file():
+    file = request.remote_addr + '.jpg'
+    return app.send_static_file(file)
 
 
 if __name__ == '__main__':
