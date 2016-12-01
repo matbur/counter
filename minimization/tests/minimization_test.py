a01c154cf5f2b10d25280c5e5214aa1243c5f438
matbur95 <mateusz@burniak.pl>
2016-12-01 11:01:50 +0100

    Add new more tests to minimization

--- minimization/tests/minimization_test.py
+++ minimization/tests/minimization_test.py
@@ -13,7 +13,12 @@ class TestMinimization(unittest.TestCase):
 
     def test_all(self):
         self.assertEqual(Minimization([4, 8, 10, 11, 12, 15], [9, 14], 'abcd').get(), 'ac + a/d + b/c/d')
+        self.assertEqual(Minimization([1, 2, 3], [0], 'ab').get(), '1')
+        self.assertEqual(Minimization([], [0], 'ab').get(), '0')
+
+    def test_all_from_data(self):
         self.assertEqual(Minimization.from_data(range(4), [0, 0, 1, 0], 'ab').get(), 'a/b')
+        self.assertEqual(Minimization.from_data(range(4), [0, 1, 0, 0], ['X1', 'X0']).get(), '/X1X0')
 
 
 if __name__ == '__main__':
